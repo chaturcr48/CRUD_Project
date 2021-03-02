c1cad8abba9e6d0666a0d4e347aed39bd96b2b30
Kumar Chatur Ranjan <chaturcr48@gmail.com>
2021-03-02 10:40:17 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- Here, I'm including bootstrap-5 css components -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Contact</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"><img
+                src="https://banner2.cleanpng.com/20180724/bhb/kisspng-logo-php-html-5b57c278415ab9.8092346315324780722677.jpg"
+                height="28px" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/CHATUR_PHP/CRUD_Project/CRUD_proj.php">Home <span
+                            class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/CHATUR_PHP/CRUD_Project/about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/CHATUR_PHP/CRUD_Project/contact.php">Contact</a>
+                </li>
+
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <div class="container mt-4 mb-4">
+        <div class="list-group" style="width: 40rem; margin: auto;">
+            <a href="https://www.instagram.com/chatur_48/" target="_blank"
+                class="list-group-item list-group-item-action list-group-item-primary">
+                <i class="fa fa-instagram" aria-hidden="true"></i>&nbsp Intagram &nbsp&nbsp&nbsp Handle -- chatur_48</a>
+            <a href="https://www.facebook.com/chatur.ranjan.1/" target="_blank"
+                class="list-group-item list-group-item-action list-group-item-primary">
+                <i class="fa fa-facebook" aria-hidden="true"></i>&nbsp&nbsp Facebook &nbsp&nbsp Handle --
+                chatur.ranjan.1</a>
+            <a href="https://twitter.com/RanjanChatur" target="_blank"
+                class="list-group-item list-group-item-action list-group-item-primary">
+                <i class="fa fa-twitter" aria-hidden="true"></i>&nbsp Twitter &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Handle --
+                @RanjanChatur</a>
+            <a href="https://github.com/chaturcr48" target="_blank"
+                class="list-group-item list-group-item-action list-group-item-primary">
+                <i class="fa fa-github" aria-hidden="true"></i>&nbsp Github &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Handle
+                -- chaturcr48</a>
+            <a href="https://www.linkedin.com/in/kumar-chatur-ranjan-538898179/" target="_blank"
+                class="list-group-item list-group-item-action list-group-item-primary">
+                <i class="fa fa-linkedin" aria-hidden="true"></i> &nbsp Linkedin &nbsp&nbsp&nbsp Handle --
+                kumar-chatur-ranjan-538898179</a>
+        </div>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+    <!-- Bootstrap-5 js components -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
